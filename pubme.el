8e130134c9234ebc1c2d01ce20bb33fa02b76a2b
Matthew Elwin <elwin@northwestern.edu>
2021-08-06 00:47:47 -0500

    Custom publisher to wrap things in the div tag
    that my css needs.
    
    Could fix the css to meet the orgmode formatting but this seems easier.

--- pubme.el
+++ pubme.el
@@ -1,12 +1,34 @@
 #!/bin/sh
-":"; # This is a shell script that also functions as a valid elisp file
-":"; # This way we can parse all arguments passed to the script and 
-":"; # Find emacs wherever it is on the path rather than a fixed location
-":"; # See https://gist.github.com/ctarbide/99b0ac9f7d6bef19cdd3e9f71b4cbcf7 for meaning of the below line
 ":"; exec emacs --quick --script "$0" -- "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
-
+;; See https://gist.github.com/ctarbide/99b0ac9f7d6bef19cdd3e9f71b4cbcf7 for meaning of the above line. 
+;; Basically, This is a shell script that also functions as a valid elisp file
+;; This way we can parse all arguments passed to the script and 
+;; Find emacs wherever it is on the path rather than a fixed location
 
 ;;; Used to publish a directory of org files to html, using my own css template
+
+(require 'org)
+(require 'ox-html)
+
+;;; 
+(defun pubme-body-filter
+    (text back-end info)
+  ;; Table of contents is wrapped in nav. 
+  
+  (format "%s</div></div>"
+          (replace-regexp-in-string "</nav>"
+                                    "</nav><div class=outer><div class=inner>" text))
+  )
+
+(org-export-define-derived-backend 'pubme-html 'html
+  :filters-alist
+  '((:filter-body . pubme-body-filter))
+  )
+
+(defun pubme-publish-to-html (plist filename pub-dir)
+  "Publish an org file to a pubme html file. Return output file name."
+  (org-publish-org-to 'pubme-html filename ".html" plist pub-dir)
+  )
 
 (defun pubme (&optional dir)
   "Publish a directory containing org files.
@@ -21,12 +43,12 @@ DIR directory containing org files that should be published as a single website.
    `("main-project"
       :base-directory ,dir
       :publishing-directory ,(concat (file-name-as-directory dir) "html")
-      :publishing-function org-html-publish-to-html
+      :publishing-function pubme-publish-to-html
       :recursive t
       :broken-links mark
-      ; Look up the following options with C-h org-<option-name-here>
       :html-doctype "html5"
       :html-html5-fancy t
+      :html-head-include-scripts nil
       :html-head-include-default-style nil
       :html-head "<link rel=\"stylesheet\" href=\"pubme.css\" type=\"text/css\"/>"
       )
@@ -52,7 +74,6 @@ DIR directory containing org files that should be published as a single website.
               display this help message and exit")
   )
 
-(require 'org)
 
 ;; if running as a script, parse the command line and call the appropriate function
 (if noninteractive
